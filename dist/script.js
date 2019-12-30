997a40b1bb6d87de88df6d5f044aa75e957c15e5
Andrey Larionov <larioandr@gmail.com>
2019-12-30 13:14:58 +0300

    Clear dist, add sketch

--- dist/script.js
+++
@@ -1,2 +0,0 @@
-"use strict";var element=document.getElementById("content");element.innerHTML='<p class="py-6 text-center text-lg italic">Site is under construction</p>';
-//# sourceMappingURL=script.js.map
