3981665b1dca7389cce5038cc69b157402a07119
Andrey Larionov <larioandr@gmail.com>
2019-12-30 04:52:39 +0300

    Draft implementation with nav, hero, about, cfp and contact sections

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";var element=document.getElementById("content");element.innerHTML='<p class="py-6 text-center text-lg italic">Site is under construction</p>';
+"use strict";var toggleCollapsingNav=function(){document.getElementById("#navCollapsingMenu").classList.toggle("hidden")};
 //# sourceMappingURL=script.js.map
